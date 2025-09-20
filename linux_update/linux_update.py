0d4b2cae4c4c78e0ee0d0acf74df19c9297943e5
Douglas Panhota <douglaspands@gmail.com>
2025-09-20 12:53:42 -0300

    script optimized

--- linux_update/linux_update.py
+++ linux_update/linux_update.py
@@ -17,14 +17,9 @@ snap list --all | awk '/disabled/{print $1, $3}' |
         """.strip()
 
     def __init__(self):
-        self._has_apt: bool | None = None
-        self._has_snap: bool | None = None
-        self._has_brew: bool | None = None
-        self._has_flatpak: bool | None = None
-        self._has_pacman: bool | None = None
-        self._has_yay: bool | None = None
         self._temp_files: list[Path] = []
-        self._startd = False
+        self._update_started = False
+        self._available: set[str] = set()
 
     def _shell(self, cmds: str | list[str], show_output: bool = True) -> int:
         cmd = " && ".join(cmds) if isinstance(cmds, list) else cmds
@@ -48,101 +43,53 @@ snap list --all | awk '/disabled/{print $1, $3}' |
             return False
 
     def _apt_upgrade(self) -> list[str]:
-        if self._has_apt is None:
-            self._has_apt = self._check_application("apt")
-        if not self._has_apt:
-            return []
         return ["sudo apt -y update", "sudo apt -y upgrade"]
 
     def _apt_clean(self):
-        if self._has_apt is None:
-            self._has_apt = self._check_application("apt")
-        if not self._has_apt:
-            return []
         return ["sudo apt -y autoremove", "sudo apt -y autoclean"]
 
     def _snap_upgrade(self):
-        if self._has_snap is None:
-            self._has_snap = self._check_application("snap")
-        if not self._has_snap:
-            return []
         return ["sudo snap refresh"]
 
     def _snap_clean(self):
-        if self._has_snap is None:
-            self._has_snap = self._check_application("snap")
-        if not self._has_snap:
-            return []
         file = Path(f"./{uuid4()}.sh")
         self._temp_files.append(file)
         file.write_text(LinuxUpdate.SH_SNAP_REMOVE)
         return [f"sudo sh {file.resolve()!s}"]
 
     def _brew_upgrade(self):
-        if self._has_brew is None:
-            self._has_brew = self._check_application("brew")
-        if not self._has_brew:
-            return []
         return [
             "brew update",
             "brew upgrade",
         ]
 
     def _brew_clean(self):
-        if self._has_brew is None:
-            self._has_brew = self._check_application("brew")
-        if not self._has_brew:
-            return []
         return ["brew cleanup"]
 
     def _flatpak_upgrade(self):
-        if self._has_flatpak is None:
-            self._has_flatpak = self._check_application("flatpak")
-        if not self._has_flatpak:
-            return []
         return [
             "flatpak update -y",
         ]
 
     def _flatpak_clean(self):
-        if self._has_flatpak is None:
-            self._has_flatpak = self._check_application("flatpak")
-        if not self._has_flatpak:
-            return []
         return [
             "flatpak uninstall --unused -y",
             "sudo rm -rfv /var/tmp/flatpak-cache-*",
         ]
 
     def _pacman_upgrade(self) -> list[str]:
-        if self._has_pacman is None:
-            self._has_pacman = self._check_application("pacman")
-        if not self._has_pacman:
-            return []
-        return ["sudo pacman -Syu --noconfirm"]
+        return ["sudo pacman --noconfirm -Syu"]
 
     def _pacman_clean(self):
-        if self._has_pacman is None:
-            self._has_pacman = self._check_application("pacman")
-        if not self._has_pacman:
-            return []
         return [
             "sudo pacman --noconfirm -Rs $(pacman -Qtdq)",
             "sudo pacman --noconfirm -Sc",
         ]
 
     def _yay_upgrade(self) -> list[str]:
-        if self._has_yay is None:
-            self._has_yay = self._check_application("yay")
-        if not self._has_yay:
-            return []
         return ["yay -Syu --noconfirm"]
 
     def _yay_clean(self):
-        if self._has_yay is None:
-            self._has_yay = self._check_application("yay")
-        if not self._has_yay:
-            return []
         return ["yay -Rs $(yay -Qtdq)", "yay --noconfirm -Sc"]
 
     def __enter__(self):
@@ -154,37 +101,43 @@ snap list --all | awk '/disabled/{print $1, $3}' |
             raise exc_value
 
     def temp_clean(self):
-        if self._startd:
+        if self._update_started:
             for file in self._temp_files:
                 file.unlink(missing_ok=True)
 
     def update(self, app: str):
-        self._startd = True
+        self._update_started = True
+        choices = self._available_managers()
         cmds = []
-        if app == "all" or app == "apt":
+        if app in ("all", "apt") and "apt" in choices:
             cmds += self._apt_upgrade() + self._apt_clean()
-        if app == "all" or app == "pacman":
+        if app in ("all", "pacman") and "pacman" in choices:
             cmds += self._pacman_upgrade() + self._pacman_clean()
-        if app == "all" or app == "yay":
+        if app in ("all", "yay") and "yay" in choices:
             cmds += self._yay_upgrade() + self._yay_clean()
-        if app == "all" or app == "snap":
+        if app in ("all", "snap") and "snap" in choices:
             cmds += self._snap_upgrade() + self._snap_clean()
-        if app == "all" or app == "flatpak":
+        if app in ("all", "flatpak") and "flatpak" in choices:
             cmds += self._flatpak_upgrade() + self._flatpak_clean()
-        if app == "all" or app == "brew":
+        if app in ("all", "brew") and "brew" in choices:
             cmds += self._brew_upgrade() + self._brew_clean()
         else:
             raise ValueError(f"Invalid app: {app}")
-        print(">> LINUX DISTRO UPDATE STARTED\n")
+
         self._shell_isolate(cmds)
-        print("\n>> LINUX DISTRO UPDATE COMPLETED")
+
+    def _available_managers(self) -> list[str]:
+        if not self._available:
+            for manager in LinuxUpdate.MANAGERS_SUPPORTED:
+                if self._check_application(manager):
+                    self._available.add(manager)
+        return list(self._available)
 
     def available_managers(self) -> list[str]:
-        available = []
-        for manager in LinuxUpdate.MANAGERS_SUPPORTED:
-            if self._check_application(manager):
-                available.append(manager)
-        return available
+        available = self._available_managers()
+        if len(available):
+            available.append("all")
+        return sorted(available)
 
 
 __all__ = ("LinuxUpdate",)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 if __name__ == "__main__":
     import argparse
+    from time import perf_counter as time
 
     from linux_update import LinuxUpdate
 
@@ -14,9 +15,16 @@ if __name__ == "__main__":
         update_parser.add_argument(
             "application",
             help="Update the application and all installed packages.",
-            choices=linux_update.available_managers() + ["all"],
+            choices=linux_update.available_managers(),
         )
 
         args = parser.parse_args()
         if args.command == "update":
+            print(">> LINUX DISTRO UPDATE STARTED\n")
+
+            time_start = time()
             linux_update.update(app=args.application)
+            time_total = time() - time_start
+
+            print(f"\n>> TOTAL TIME TAKEN: {time_total:.2f} seconds")
+            print(">> LINUX DISTRO UPDATE COMPLETED")
